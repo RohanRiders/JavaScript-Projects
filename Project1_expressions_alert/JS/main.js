b2ab42d7b5ab2b09a59efb4e3a9de5fe2d3e8835
RohanRiders <159487815+RohanRiders@users.noreply.github.com>
2024-03-23 20:32:29 -0600

    initial commit for my JS work.

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+/*window.alert("Hello World!"); //This writes the string "Hello World" to the browser screen
+document.write('Hello, World!');* //This is another function that writes the string to the browser screen
+
+/*var A = "This is a string"; //This is a variable that assigns a string to it. 
+document.write(A)*/ //This is the function that writes the string to the screen. 
+
+/*var X = "I have a lovely bunch of coconuts" //Assigning a string to a variable. 
+window.alert(X)*/
+
+/*window.alert("This is my brother\'s car, I need to return it to him or, \"He\'ll chase me down and gut me like a fish\".");*/ //Practice escaping special characters like apostrophes. 
+
+/*document.write("Be the change"
++ " you want to see in the world");*/ // practices concatination
+
+/*var X = "The shepard boy to the might king," + " \"Do you hear what I hear?\"" //practices setting a variable with concatination and using that variable in the funciton
+document.write(X);*/
+
+/*var Family = "The Aressinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro"; //Practicing that you can assign multiple variables to one declaration 
+document.write(Dad);*/ 
+
+// window.alert(6*6); //Practices the difference between an expression and a statement. 
+
+var sent1 = "This is the beginning of the string"; //setting a string to a variable.
+var sent2 = " and this is the end of the string"; //setting the rest of the string to another variable. 
+document.write(sent1 + sent2); //writing an expression
